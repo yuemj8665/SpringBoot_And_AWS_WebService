7cd548b1a7091c8679a20a815a8f9b02ba4f70d1
MJ <yuemjmahu@gmail.com>
2020-05-12 00:32:22 +0900

    Error complate

--- src/main/java/com/mjmj/book/springboot/Application.java
+++ src/main/java/com/mjmj/book/springboot/Application.java
@@ -1,8 +1,8 @@
 package com.mjmj.book.springboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
